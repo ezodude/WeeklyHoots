852633f0297fe64810af919a05bd44d3bcc2b3dc
Abdel A Saleh <asaleh@the-experimenters.com>
2011-08-31 22:01:38 +0100

    Initial attempt at re-working the storybox date duration.

---
+++ SuperOwlWeeklyHoots/AboutViewController.h
@@ -0,0 +1,16 @@
+//
+//  SettingsViewController.h
+//  SuperOwlWeeklyHoots
+//
+//  Created by Abdel Saleh on 31/08/2011.
+//  Copyright 2011 Said.fm Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SettingsViewController : UIViewController {
+    
+}
+
+@end
---
+++ SuperOwlWeeklyHoots/SettingsNavController.h
@@ -0,0 +1,16 @@
+//
+//  SettingsNavController.h
+//  SuperOwlWeeklyHoots
+//
+//  Created by Abdel Saleh on 31/08/2011.
+//  Copyright 2011 Said.fm Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SettingsNavController : UIViewController {
+    
+}
+
+@end
