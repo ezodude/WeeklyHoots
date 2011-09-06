afd3aea8e175fc995cb8c0f1cb0f206de9768003
Abdel A Saleh <asaleh@the-experimenters.com>
2011-09-06 17:30:54 +0100

    Added Alpha info section.

---
+++ SuperOwlWeeklyHoots/TrialInfoViewController.h
@@ -0,0 +1,16 @@
+//
+//  TrialInfoViewController.h
+//  SuperOwlWeeklyHoots
+//
+//  Created by Abdel Saleh on 06/09/2011.
+//  Copyright 2011 Said.fm Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TrialInfoViewController : UIViewController {
+    
+}
+
+@end
